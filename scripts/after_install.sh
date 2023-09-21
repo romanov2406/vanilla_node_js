25cd35c9f89f0c9e8eec5b815436d921eafc6020
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-09-21 21:00:50 +0100

    Version 1

---
+++ scripts/after_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo 'run after_install.sh: ' >> /var/www/vanilla_node_js/deploy.log
+
+#echo 'cd /var/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+#cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+
+echo 'npm install' >> /var/www/vanilla_node_js/deploy.log
+npm install >> /var/www/vanilla_node_js/deploy.log
