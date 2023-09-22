3d198e18ce835a703439b468f6276762bcb62cbb
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-09-22 16:08:05 +0100

    Version 1

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /var/www/vanilla_node_js/deploy.log
-
-echo 'forever restartall' >> /var/www/vanilla_node_js/deploy.log
-forever restartall >> /var/www/vanilla_node_js/deploy.log
+#echo 'run application_start.sh: ' >> /var/www/vanilla_node_js/deploy.log
+#
+#echo 'forever restartall' >> /var/www/vanilla_node_js/deploy.log
+cd /var/www/vanilla_node_js
+forever restartall
