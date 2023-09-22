7527383e21399ff3939ed4648060976704424b06
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-09-22 22:07:25 +0100

    Version 1

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const app = express();
 let count = 0;
 
 app.get("/", async (req, res) => {
-  res.send("RNDK24 TEST SERVER ::::::: " + ++count +  '<span style="color: yellow; font-style: italic"> PINK VERSION 1</span>');
+  res.send(
+    "RNDK24 TEST SERVER ::::::: " +
+      ++count +
+      '<span style="color: yellow; font-style: italic"> PINK VERSION 1</span>'
+  );
 });
-
-
 
 app.listen(3000);
