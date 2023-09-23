d152f47c9e6179e8bfea425cbcb7f770633af148
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-09-23 19:37:11 +0100

    Version 1

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ app.get("/", async (req, res) => {
   res.send(
     "RNDK24 TEST SERVER ::::::: " +
       ++count +
-      '<span style="color: red; font-style: italic"> PINK VERSION 3</span>'
+      '<span style="color: palegoldenrod; font-style: italic"> PINK VERSION 3</span>'
   );
 });
 
