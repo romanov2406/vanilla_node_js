062432067a600685924064b513f99cd317996598
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-09-24 15:36:01 +0100

    Version 1

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,12 @@
-forever restartall
+cd /var/www/vanilla_node_js
+
+
+/usr/local/bin/forever start main.js
+
+# Check if the application started successfully.
+if [ $? -eq 0 ]; then
+   echo "Node.js application started successfully."
+else
+   echo "Error: Failed to start Node.js application."
+   exit 1
+fi
