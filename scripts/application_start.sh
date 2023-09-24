d258e616c6b91b5b348d0a0e3b8347f26566ea42
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-09-24 14:37:32 +0100

    Version 1

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,13 +1,6 @@
-
-
-#chmod -R 777 cd /var/www/vanilla_node_js
-#cd /var/www/vanilla_node_js
-#npm i
-#forever restartall
-
 echo 'npm install' >> /var/www/vanilla_node_js
 npm install >> /var/www/vanilla_node_js
 
 
-echo 'forever restartall' >> /var/www/vanilla_node_js
-forever restartall >> /var/www/vanilla_node_js
+#echo 'forever restartall' >> /var/www/vanilla_node_js
+#forever restartall >> /var/www/vanilla_node_js
