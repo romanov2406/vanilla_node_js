931ff05b33cedfb78fe3abbd3528720bba497b0e
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-09-24 17:41:00 +0100

    Version 2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,18 +1,21 @@
+#load local bash profile
+source /home/ec2-user/.bash_profile
 
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
 
 sudo chmod -R 777 /var/www/vanilla_node_js
 
 cd /var/www/vanilla_node_js
 
 # add npm and node to path
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 # source ~/.nvm/nvm.sh
 
 npm install
 
-forever stopall
-forever start index.js
+sudo forever stopall
+sudo forever start main.js
