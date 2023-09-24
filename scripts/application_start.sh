6eefdbd6c9275e1f6aa338eff190649fe03a4458
romanov2406 <71198724+romanov2406@users.noreply.github.com>
2023-09-24 17:23:19 +0100

    Version 1

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+# Debugging: Print the current PATH
+echo "Current PATH: $PATH"
+
+# Navigate to your application directory.
 cd /var/www/vanilla_node_js
-npm install
-cd scripts
-bash start.sh
+
+# Specify the full path to the 'forever' command.
+/usr/local/bin/forever start main.js
+
+# Check if the application started successfully.
+if [ $? -eq 0 ]; then
+   echo "Node.js application started successfully."
+else
+   echo "Error: Failed to start Node.js application."
+   exit 1
+fi
